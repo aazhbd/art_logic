d742e1c34ab045e8a108866932c679d8e407325d
Abdullah Al Zakir Hossain <aazhbd@conveylive.com>
2021-11-22 21:31:10 +0100

    Added home page.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,39 @@
 from django.db import models
+import datetime
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+    date = models.DateField("Date posted", default=datetime.date.today)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body', classname="full"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        ImageChooserPanel('feed_image'),
+    ]
+
+
