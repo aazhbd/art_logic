0e34c2c4ec753cbb2bc7c762543a0428fae5b4a4
Abdullah Al Zakir Hossain <aazhbd@conveylive.com>
2021-12-22 12:23:26 +0100

    Page designs and layouts updated.

--- content/content/urls.py
+++ content/content/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 from search import views as search_views
 
@@ -12,8 +13,9 @@ urlpatterns = [
     path('django-admin/', admin.site.urls),
 
     path('admin/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
+    path('sitemap.xml', sitemap),
 
+    path('documents/', include(wagtaildocs_urls)),
     path('search/', search_views.search, name='search'),
 
 ]
