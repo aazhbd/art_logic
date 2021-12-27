2c4c411dbf80b3ac5293a78a623c243d507902b1
Abdullah Al Zakir Hossain <aazhbd@conveylive.com>
2021-12-27 20:08:39 +0100

    Page designs and layouts updated.

--- content/content/settings/production.py
+++ content/content/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+SECRET_KEY = 'django-insecure-+c$%+(&oaw(0x53ah@#1$s)-xh7r#d#d#$5q*k*ujn%7gt*_y2'
+
+ALLOWED_HOSTS = ['articulatedlogic.com']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
     from .local import *
 except ImportError:
