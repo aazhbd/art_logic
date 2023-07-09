8917ef5ac1ccd55a89cca29fa6a9d4b5f77d07c5
Abdullah Al Zakir Hossain <aazhbd@yahoo.com>
2023-07-09 15:27:18 +0200

    Updated compatibility packages.

--- content/home/models.py
+++ content/home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
 
 from wagtailmarkdown.edit_handlers import MarkdownPanel
